0801aa1131386f30a18725d742f1da1b6c774c4e
ISongwut <isongwut.me@gmail.com>
2024-06-07 18:28:10 +0700

    add: book repository

---
+++ internal/books/repository/book_repository.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"github.com/ISongwuts/go-crud-elasticsearch/internal/models"
+)
+
+type (
+	IBookRepository interface {
+		Create(user *models.Book) (string, error)
+		GetUsers() ([]models.Book, error)
+		GetByID(id string) (models.Book, error)
+		Update(id string, modify map[string]string) (string, error)
+		Delete(id string) (string, error)
+	}
+
+	BookRepository struct {}
+)
+
+func NewUserRepository() IBookRepository {
+	return &BookRepository{}
+}
+
+func (u *BookRepository) Create(users *models.Book) (string, error) {
+	return "", nil
+}
+
+func (u *BookRepository) GetUsers() ([]models.Book, error) {
+	users := make([]models.Book, 0)
+	return users, nil
+}
+
+func (u *BookRepository) GetByID(id string) (models.Book, error) {
+	user := models.Book{}
+	return user, nil
+}
+
+func (u *BookRepository) Update(id string, modify map[string]string) (string, error){
+	return "", nil
+}
+
+func (u *BookRepository) Delete(id string) (string, error) {
+	return "", nil
+}
