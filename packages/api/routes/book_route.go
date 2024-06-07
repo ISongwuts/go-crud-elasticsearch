5dd1df2b362af0616a540d6189ac6f3bcc29f76f
ISongwut <isongwut.me@gmail.com>
2024-06-07 22:44:03 +0700

    add: book route

---
+++ packages/api/routes/book_route.go
@@ -0,0 +1,19 @@
+package routes
+
+import (
+	"github.com/ISongwuts/go-crud-elasticsearch/internal/books/usecase"
+	"github.com/ISongwuts/go-crud-elasticsearch/packages/api/controllers"
+	"github.com/gin-gonic/gin"
+)
+
+func BookRouter(rg *gin.RouterGroup) {
+	bookUsecase := usecase.BookUsecase{}
+	controller := controllers.NewController(&bookUsecase)
+	
+	//Route paths
+	rg.GET("/books", controller.GetAll)
+	rg.GET("/book/:id", controller.GetOnce)
+	rg.POST("/books", controller.Create)
+	rg.PUT("/book/:id", controller.Update)
+	rg.DELETE("/book/:id", controller.Delete)
+}
