2a747292080913a4f8951adac7f43e104713a87d
ISongwut <isongwut.me@gmail.com>
2024-06-07 17:09:49 +0700

    add: book & user models

---
+++ internal/models/book.go
@@ -0,0 +1,9 @@
+package models
+
+type Book struct {
+	ID 			string	`json:"book_id"`
+	Title 		string 	`json:"book_title"`
+	Description string 	`json:"book_desc"`
+	Author 		string 	`json:"book_author"`
+	Price		string	`json:"book_price"`
+}
