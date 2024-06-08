c19fdc678a42e09d3d4b55509509256210762b40
ISongwut <isongwut.me@gmail.com>
2024-06-08 07:25:13 +0700

    add: elasticsearch config

--- config/config.go
+++ config/config.go
@@ -1,8 +1,14 @@
 package config
 
+import (
+	"github.com/elastic/go-elasticsearch/v7"
+	"os"
+)
+
 type (
 	IConfig interface {
 		GetRouteRelativePath() string
+		ElasticSearchConfig() (*elasticsearch.Client, error)
 	}
 
 	Config struct {}
@@ -14,4 +20,15 @@ func NewConfig() IConfig {
 
 func (* Config) GetRouteRelativePath() string {
 	return "/api/"
+}
+
+func (* Config) ElasticSearchConfig() (*elasticsearch.Client, error){
+	host := os.Getenv("ES_HOST")
+	config := elasticsearch.Config{
+		Addresses: []string{
+			host,
+		},
+	}
+	cli, err := elasticsearch.NewClient(config)
+	return cli, err
 }
